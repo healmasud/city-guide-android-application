6af02d41f3e18480e5625fb9ca3610c68134c145
Md Abdulla Heal Masud <heal.masud@gmail.com>
2022-08-31 23:14:57 +0300

    home featured content

--- app/src/main/java/com/healmasud/cityguide/User/UserDashboard.java
+++ app/src/main/java/com/healmasud/cityguide/User/UserDashboard.java
@@ -1,18 +1,48 @@
 package com.healmasud.cityguide.User;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import com.healmasud.cityguide.HelperClasses.HomeAdapter.FeaturedAdpater;
+import com.healmasud.cityguide.HelperClasses.HomeAdapter.FeaturedHelperClass;
 import com.healmasud.cityguide.R;
 
+import java.util.ArrayList;
+
 public class UserDashboard extends AppCompatActivity {
+
+    RecyclerView featuredRecycler;
+    RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.user_dashboard);
+
+        //Hooks
+        featuredRecycler = findViewById(R.id.featured_recycler);
+
+        featuredRecycler();
+    }
+
+    private void featuredRecycler() {
+
+        featuredRecycler.setHasFixedSize(true);
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
+        ArrayList<FeaturedHelperClass> featuredLocations = new ArrayList<>();
+
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.mcdonalds_img,"Mcdonald's", "This is just a placeholder description"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.kfc,"KFC", "KFC, This is just a placeholder description"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.pizza_hut,"Pizza Hut", "Pizza, This is just a placeholder description"));
+
+        adapter = new FeaturedAdpater(featuredLocations);
+        featuredRecycler.setAdapter(adapter);
+
     }
 }
