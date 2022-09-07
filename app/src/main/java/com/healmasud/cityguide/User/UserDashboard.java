6227d5d60747ecd12cc393053def228965ef6b7b
Md Abdulla Heal Masud <heal.masud@gmail.com>
2022-09-07 19:57:05 +0300

    initialized drawer layout

--- app/src/main/java/com/healmasud/cityguide/User/UserDashboard.java
+++ app/src/main/java/com/healmasud/cityguide/User/UserDashboard.java
@@ -1,12 +1,14 @@
 package com.healmasud.cityguide.User;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import com.google.android.material.navigation.NavigationView;
 import com.healmasud.cityguide.HelperClasses.HomeAdapter.CategoriesAdpater;
 import com.healmasud.cityguide.HelperClasses.HomeAdapter.CategoriesHelperClass;
 import com.healmasud.cityguide.HelperClasses.HomeAdapter.FeaturedAdpater;
@@ -19,12 +21,15 @@ import java.util.ArrayList;
 
 public class UserDashboard extends AppCompatActivity {
 
+    // Variables
     RecyclerView featuredRecycler;
+    RecyclerView mv_recycler;
+    RecyclerView categories_recycler;
     RecyclerView.Adapter adapter;
 
-    RecyclerView mv_recycler;
-
-    RecyclerView categories_recycler;
+    // drawer menu
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,21 +42,25 @@ public class UserDashboard extends AppCompatActivity {
         mv_recycler = findViewById(R.id.mv_recycler);
         categories_recycler = findViewById(R.id.categories_recycler);
 
+        // Menu hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+
+        // Recycler views function calls
         featuredRecycler();
-
         mv_recycler();
-
         categories_recycler();
     }
 
     private void categories_recycler() {
         categories_recycler.setHasFixedSize(true);
-        categories_recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        categories_recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
         ArrayList<CategoriesHelperClass> categoriesLocations = new ArrayList<>();
 
-        categoriesLocations.add(new CategoriesHelperClass(R.drawable.restaurant_image,"Restaurants"));
-        categoriesLocations.add(new CategoriesHelperClass(R.drawable.city,"Cities"));
+        categoriesLocations.add(new CategoriesHelperClass(R.drawable.restaurant_image, "Restaurants"));
+        categoriesLocations.add(new CategoriesHelperClass(R.drawable.city, "Cities"));
 
         adapter = new CategoriesAdpater(categoriesLocations);
         categories_recycler.setAdapter(adapter);
@@ -61,29 +70,28 @@ public class UserDashboard extends AppCompatActivity {
 
     private void mv_recycler() {
         mv_recycler.setHasFixedSize(true);
-        mv_recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        mv_recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
         ArrayList<MVHelperClass> MVLocations = new ArrayList<>();
 
-        MVLocations.add(new MVHelperClass(R.drawable.pizza_hut,"Pizza Hut", "Pizza, This is just a placeholder description"));
-        MVLocations.add(new MVHelperClass(R.drawable.mcdonalds_img,"Mcdonald's", "This is just a placeholder description"));
-        MVLocations.add(new MVHelperClass(R.drawable.kfc,"KFC", "KFC, This is just a placeholder description"));
+        MVLocations.add(new MVHelperClass(R.drawable.pizza_hut, "Pizza Hut", "Pizza, This is just a placeholder description"));
+        MVLocations.add(new MVHelperClass(R.drawable.mcdonalds_img, "Mcdonald's", "This is just a placeholder description"));
+        MVLocations.add(new MVHelperClass(R.drawable.kfc, "KFC", "KFC, This is just a placeholder description"));
 
         adapter = new MVAdpater(MVLocations);
         mv_recycler.setAdapter(adapter);
     }
 
-
     private void featuredRecycler() {
 
         featuredRecycler.setHasFixedSize(true);
-        featuredRecycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
         ArrayList<FeaturedHelperClass> featuredLocations = new ArrayList<>();
 
-        featuredLocations.add(new FeaturedHelperClass(R.drawable.mcdonalds_img,"Mcdonald's", "This is just a placeholder description"));
-        featuredLocations.add(new FeaturedHelperClass(R.drawable.kfc,"KFC", "KFC, This is just a placeholder description"));
-        featuredLocations.add(new FeaturedHelperClass(R.drawable.pizza_hut,"Pizza Hut", "Pizza, This is just a placeholder description"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.mcdonalds_img, "Mcdonald's", "This is just a placeholder description"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.kfc, "KFC", "KFC, This is just a placeholder description"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.pizza_hut, "Pizza Hut", "Pizza, This is just a placeholder description"));
 
         adapter = new FeaturedAdpater(featuredLocations);
         featuredRecycler.setAdapter(adapter);
